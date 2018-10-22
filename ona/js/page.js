dfd8431c61646d13f53fcc61cba9eec118048a3c
Ivan K <ivan.kocienski@gmail.com>
2018-10-22 14:23:11 +0100

    New ONA version

--- ona/js/page.js
+++ ona/js/page.js
@@ -1,37 +1,28 @@
 
 $(function() {
 
-  $("#zone-1 a.scroll-down").click(function() {
-      $([document.documentElement, document.body]).animate({
-          scrollTop: $("#zone-2").offset().top
-      }, 200);
+  // methods and values
+  $('.cell .clicker').click(
+    function(event) {
+      console.log("clicker clicked");
 
-    return false;
+      var target = $(event.target);
+      var parent = target.parent(); 
+      var child = parent.find('.hover');
+      child.fadeToggle(200);
+    });
+
+  // page scroller
+  $('#zone-holder').pagepiling({
+    menu: '#menu-links',
+    anchors: [ 'zone-1', 'zone-2', 'zone-3', 'zone-4', 'zone-5', 'zone-6' ],
+    scrollingSpeed: 300,
+    //navigation: {
+    //  'position': 'right',
+    //  'tooltips': [ 'About', 'Methods', 'Clients', 'Video', 'Case Studies', 'Contact' ]
+    //}
   });
 
-  $("#zone-2 a.scroll-down").click(function() {
-      $([document.documentElement, document.body]).animate({
-          scrollTop: $("#zone-3").offset().top
-      }, 200);
-
-    return false;
-  });
-
-  $("#zone-3 a.scroll-down").click(function() {
-      $([document.documentElement, document.body]).animate({
-          scrollTop: $("#zone-4").offset().top
-      }, 200);
-
-    return false;
-  });
-
-  $("#zone-4 a.scroll-down").click(function() {
-      $([document.documentElement, document.body]).animate({
-          scrollTop: $("#zone-5").offset().top
-      }, 200);
-
-    return false;
-  });
 
   console.log("page loaded");
 });
