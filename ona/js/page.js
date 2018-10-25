1b20f49e83cde964b2d9bc89e5d3e7c1afd93e66
Ivan K <ivan.kocienski@gmail.com>
2018-10-25 12:30:49 +0100

    Mobile version of ONA

--- ona/js/page.js
+++ ona/js/page.js
@@ -1,27 +1,31 @@
 
 $(function() {
 
-  // methods and values
-  $('.cell .clicker').click(
-    function(event) {
-      console.log("clicker clicked");
+  if(false) {
+    // methods and values
+    $('.cell .clicker').click(
+      function(event) {
+        console.log("clicker clicked");
 
-      var target = $(event.target);
-      var parent = target.parent(); 
-      var child = parent.find('.hover');
-      child.fadeToggle(200);
+        var target = $(event.target);
+        var parent = target.parent(); 
+        var child = parent.find('.hover');
+        child.fadeToggle(200);
+      });
+    }
+
+  if(false) {
+    // page scroller
+    $('#zone-holder').pagepiling({
+      menu: '#menu-links',
+      anchors: [ 'zone-1', 'zone-2', 'zone-3', 'zone-4', 'zone-5', 'zone-6' ],
+      scrollingSpeed: 300,
+      //navigation: {
+      //  'position': 'right',
+      //  'tooltips': [ 'About', 'Methods', 'Clients', 'Video', 'Case Studies', 'Contact' ]
+      //}
     });
-
-  // page scroller
-  $('#zone-holder').pagepiling({
-    menu: '#menu-links',
-    anchors: [ 'zone-1', 'zone-2', 'zone-3', 'zone-4', 'zone-5', 'zone-6' ],
-    scrollingSpeed: 300,
-    //navigation: {
-    //  'position': 'right',
-    //  'tooltips': [ 'About', 'Methods', 'Clients', 'Video', 'Case Studies', 'Contact' ]
-    //}
-  });
+  }
 
 
   console.log("page loaded");
